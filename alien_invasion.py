3e24d594d604de9b64f8f4de1e990253d678a5ac
sashakukhtyk <sashakukhtyk@users.noreply.github.com>
2024-08-04 11:17:41 +0300

    refactor alien addition, create new helper method _create_alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -107,10 +107,14 @@ class AlienInvasion:
         # Create the first row of aliens
         for alien_number in range(number_aliens_x):
             # Create an alien and put it in a row
-            alien = Alien(self)
-            alien.x = alien_width + 2 * alien_width * alien_number
-            alien.rect.x = alien.x
-            self.aliens.add(alien)
+            self._create_alien(alien_number)
+
+    def _create_alien(self, alien_number):
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        self.aliens.add(alien)
 
 
 if __name__ == '__main__':
