80ff087dac6720e704752835658dfd89cac4228b
sashakukhtyk <sashakukhtyk@users.noreply.github.com>
2024-08-04 14:12:51 +0300

    minor changes to the bullet speed

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ class Settings:
         self.ship_speed = 1.5
 
         # Bullet settings
-        self.bullet_speed = 1
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
@@ -19,5 +19,6 @@ class Settings:
         # Alien settings
         self.alien_speed = 1.0
         self.fleet_drop_speed = 10
+
         # Fleet direction 1 means right, -1 means left
         self.fleet_direction = 1
