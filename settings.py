44a6e3eeb66a4e0a52e5ce9b9d7060acca955518
sashakukhtyk <sashakukhtyk@users.noreply.github.com>
2024-08-04 10:43:06 +0300

    change the color of the screen and add alien.bmp

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ class Settings:
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (173, 216, 230)
+        self.bg_color = (230, 230, 230)
 
         # Ship settings
         self.ship_speed = 1.5
