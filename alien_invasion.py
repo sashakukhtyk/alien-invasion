a31458b508febd64e8c0ae8dfac0d601aebbd032
sashakukhtyk <sashakukhtyk@users.noreply.github.com>
2024-08-05 14:12:00 +0300

    play button is working only when the game is inactive

--- alien_invasion.py
+++ alien_invasion.py
@@ -82,7 +82,8 @@ class AlienInvasion:
 
     def _check_play_button(self, mouse_pos):
         """Start new game when Play button is pressed"""
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
             # Empty game stats
             self.stats.reset_stats()
             self.stats.game_active = True
