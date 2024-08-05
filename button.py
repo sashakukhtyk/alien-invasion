771438aa116ccca3f728d5a85c755520ba4802ea
sashakukhtyk <sashakukhtyk@users.noreply.github.com>
2024-08-05 12:25:21 +0300

    adding the attributes of the button class

--- button.py
+++ button.py
@@ -1,3 +1,24 @@
 import pygame.font
 
+
 class Button:
+    def __init__(self, ai_game, msg):
+        """Initialize button attributes"""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Define size and type of button
+        self.width, self.height = 200, 50
+        self.button_color = (0, 255, 0)
+        self.text_color = (255, 255, 255)
+        self.font = pygame.font.SysFont(None, 48)
+
+        # Create a button object and center it
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        self.rect.center = self.screen_rect.center
+
+        # Show the message on the button
+        self._prep_msg(msg)
+
+        def _prep_msg(self, msg):
+            pass
