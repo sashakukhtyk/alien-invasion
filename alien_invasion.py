2613caec2fdc5b384af200af435fc358d6796aab
sashakukhtyk <sashakukhtyk@users.noreply.github.com>
2024-08-05 14:05:08 +0300

    play button is working now

--- alien_invasion.py
+++ alien_invasion.py
@@ -76,6 +76,15 @@ class AlienInvasion:
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
 
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
+    def _check_play_button(self, mouse_pos):
+        """Start new game when Play button is pressed"""
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.stats.game_active = True
+
     def _check_keydown_events(self, event):
         """Respond to keydown events."""
         if event.key == pygame.K_RIGHT:
