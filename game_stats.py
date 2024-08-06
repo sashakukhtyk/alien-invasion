b7738c83af89eb9d61de7e9b3339301d861610b9
sashakukhtyk <sashakukhtyk@users.noreply.github.com>
2024-08-06 12:58:27 +0300

    high score is implemented now

--- game_stats.py
+++ game_stats.py
@@ -8,6 +8,9 @@ class GameStats:
         # Start the active game
         self.game_active = False
 
+        # High score initialization
+        self.high_score = 0
+
     def reset_stats(self):
         """Initialize statistics of the game, that can be changed through the game"""
         self.ships_left = self.ai_settings.ship_limit
